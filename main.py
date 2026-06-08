e26f016c5647c3bf1411de72852bd743a570adcf
aryangarg1803-create <aryangarg1803@gmail.com>
2026-06-08 13:44:47 +0530

    Initialize Discord gambling bot repository

---
+++ main.py
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Discord gambling bot"
+git branch -M main
+git remote add origin https://github.com/aryangarg1803-create/LuckyBet2.git
+git push -u origin main
